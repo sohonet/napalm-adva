1b60337517eece8dfc70761c8614a4f2316e0932
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2023-07-24 10:34:47 +0100

    Handle timeout=None being passed to driver

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as fs:
 
 setup(
     name="napalm-adva",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Johan van den Dorpe",
     author_email="johan@vdltech.net",
